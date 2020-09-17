340d23f99b6b75a8615c58d52adcfe963f5d27db
thorpe <koangbok@gmail.com>
2020-09-17 19:47:18 +0900

    fixed usrsctp

--- 10_janus.sh
+++ 10_janus.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/meetecho/janus-gateway.git
 cd janus-gateway
 sh autogen.sh
-./configure --prefix=/opt/janus --disable-rabbitmq --disable-mqtt
+./configure --prefix=/opt/janus
 make
 make install
 make configs
